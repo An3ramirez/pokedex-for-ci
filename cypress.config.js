79cbe27ef38ac5ea9cdf4257c0b2dc08fddb0227
Andres Felipe Ramirez <an3ramirez@gmail.com>
2023-06-29 19:29:49 -0500

    update cypress library

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:500',
+    baseUrl: 'http://localhost:5000',
   },
   video: false,
 })
